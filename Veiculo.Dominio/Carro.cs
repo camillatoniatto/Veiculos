07d511b4751b4a8c465a92c39d66ba36b267931c
Camilla Toniatto <camillatoniatto@hotmail.com>
2021-10-06 17:18:24 -0300

    Adicionar arquivos de projeto.

---
+++ Veiculo.Dominio/Carro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Veiculo.Dominio
+{
+    public class Carro
+    {
+        public int Id { get; set; }
+        public string Modelo { get; set; }
+        public string Placa { get; set; }
+        public int Ano { get; set; }
+        public List<Reserva> Reservas { get; set; }
+    }
+}
---
+++ Veiculo.Dominio/Reserva.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Veiculo.Dominio
+{
+    public class Reserva
+    {
+        public int Id { get; set; }
+        public DateTime DtInicio { get; set; }
+        public DateTime DtFim { get; set; }
+        public Carro Carro { get; set; }
+    }
+}
